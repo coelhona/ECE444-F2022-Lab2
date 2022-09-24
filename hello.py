f3782a816e9187447fad2b2178938430621b8ce1
Nathan Coelho <nathan.coelho@mail.utoronto.ca>
2022-09-24 15:15:46 -0400

    added templates folder and created user.html file

--- hello.py
+++ hello.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}!!</h2>'.format(name)
+    return render_template('user.html', name=name)
