cca871fa4ce67f6acda4a086ab5c2f59e0f69bf0
Nathan Coelho <nathan.coelho@mail.utoronto.ca>
2022-09-25 17:25:28 -0400

    Completed step 1: modified base and index templates and hello.py file for Name form and validation

--- hello.py
+++ hello.py
@@ -1,15 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 app = Flask(__name__)
+app.config["SECRET_KEY"] = 'wagwan'
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data #storing name in user sessions so that it is remembered after the request has been made
+        return redirect(url_for('index')) #calling redirect to avoid refresh confirmation warning from a repeat POST request
+    return render_template('index.html', form=form, name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
